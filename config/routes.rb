d0c31e1a3125aa04c342bf1509365c19ed635934
ionajosephine <ionajosephine@gmail.com>
2022-04-13 20:55:27 +0200

    Tidying custom button class &rmv duplication in tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root "recipes#index"
   devise_for :contributors, :controllers => { registrations: 'contributors/registrations' }
-  #get '/contributors', to: '/contributors#index'
-
   resources :contributors, only: [:index, :show]
   resources :recipes
 end
