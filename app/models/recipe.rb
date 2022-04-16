0e0835b83cd9cccb4c5a509960c8451d625efbb0
ionajosephine <ionajosephine@gmail.com>
2022-04-16 10:15:07 +0200

    Add Ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   belongs_to :contributor, counter_cache: true
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_15_182858) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_16_074959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_182858) do
     t.index ["reset_password_token"], name: "index_contributors_on_reset_password_token", unique: true
   end
 
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.bigint "recipe_id", null: false
+    t.bigint "ingredient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", null: false
@@ -38,5 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_182858) do
     t.index ["contributor_id"], name: "index_recipes_on_contributor_id"
   end
 
+  add_foreign_key "recipe_ingredients", "ingredients"
+  add_foreign_key "recipe_ingredients", "recipes"
   add_foreign_key "recipes", "contributors"
 end
