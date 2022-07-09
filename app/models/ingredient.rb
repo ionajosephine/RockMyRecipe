f25b4f854d39686a8be391233f5766432c8b6861
ionajosephine <ionajosephine@gmail.com>
2022-07-09 16:58:30 +0100

    Remove code from legacy recipe_ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,4 @@
 class Ingredient < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   belongs_to :recipe
-  # has_many :recipe_ingredients
-  # has_many :recipes, through: :recipe_ingredients
 end
