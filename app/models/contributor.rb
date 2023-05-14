d28465ba7d6f19cc2532b62c16ba9433976d1660
Iona Meadows <81577041+ionajosephine@users.noreply.github.com>
2023-05-14 20:26:52 +0200

    Access (#26)
    * Remove sign_up
    
    * Minor update

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -2,7 +2,7 @@ class Contributor < ApplicationRecord
   # Include default devise modules. Others available are:
   # :trackable and :omniauthable and :confirmable, :lockable, :timeoutable
 
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable 
   validates :profile_name, presence: true, uniqueness: true
   has_many :recipes, :dependent => :destroy
